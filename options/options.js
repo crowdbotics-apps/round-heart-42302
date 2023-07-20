0808f7f58d62b3929ebc352c2f25df21d099401c
Crowdbotics <team@crowdbotics.com>
2023-07-20 13:59:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-heart-42302",
+  url: "https://round-heart-42302.botics.co",
+  api: "https://round-heart-42302.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
