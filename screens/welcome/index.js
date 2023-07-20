a14bf8c7bdc07a07052ec5c509b0c38954eb73a4
Quentin Renouf <quentinthibaut.renouf@gmail.com>
2023-07-20 10:31:54 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,22 +1,25 @@
-import React from "react"
-import { View, Image, Text, ScrollView, SafeAreaView } from "react-native"
-import { styles } from "./styles"
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import React from "react";
+import { View, Image, Text, ScrollView, SafeAreaView } from "react-native";
+import { styles } from "./styles";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  const navigation = useNavigation();
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
         <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
+          <Pressable onPress={() => {
+          navigation.navigate("aboutTheApp");
+        }}><Image style={styles.logo} source={require("./logo.png")} /></Pressable>
           <Text style={styles.text}>
             Let's build something amazing together!
           </Text>
         </View>
         <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
       </ScrollView>
-    </SafeAreaView>
-  )
-}
+    </SafeAreaView>;
+};
 
-export default WelcomeScreen
+export default WelcomeScreen;
--- store/roundheartAPI/api.js
+++ store/roundheartAPI/api.js
@@ -1,55 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const roundheartAPI = axios.create({
   baseURL: "https://round-heart-42302.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return roundheartAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return roundheartAPI.post(`/api/v1/login/`, payload.data)
+  return roundheartAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return roundheartAPI.post(`/api/v1/signup/`, payload.data)
+  return roundheartAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return roundheartAPI.post(`/rest-auth/login/`, payload.data)
+  return roundheartAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return roundheartAPI.get(`/rest-auth/logout/`)
+  return roundheartAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return roundheartAPI.post(`/rest-auth/logout/`)
+  return roundheartAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return roundheartAPI.post(`/rest-auth/password/change/`, payload.data)
+  return roundheartAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return roundheartAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return roundheartAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return roundheartAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return roundheartAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return roundheartAPI.post(`/rest-auth/registration/`, payload.data)
+  return roundheartAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return roundheartAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return roundheartAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return roundheartAPI.get(`/rest-auth/user/`)
+  return roundheartAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return roundheartAPI.put(`/rest-auth/user/`, payload.data)
+  return roundheartAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return roundheartAPI.patch(`/rest-auth/user/`, payload.data)
+  return roundheartAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -65,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
